4dae3d553584a2bbea80a690720de0cba84a3780
beth050201 <56397955+beth050201@users.noreply.github.com>
2020-04-09 14:32:42 +0100

    content

--- main.js
+++ main.js
@@ -13,15 +13,14 @@ const navSlide= ()=> {
     });
 
 
-  // Fixed an issue here from foreach to forEach.
+  
     navLinks.forEach((link,index) =>{
        if(link.style.animation) {
            link.style.animation=''
         } else {
-          //Here there was a small mistake of using normal quotes '' and not back ticks ``.
-          //Thats it!
-            link.style.animation=`navLinkFade 0.5s ease forwards ${index /7 + 1.5}s`;
+          link.style.animation=`navLinkFade 0.5s ease forwards ${index /7 + 1}s`;
         }
+        // shows the delay for each link
         console.log(index / 7);
     });
 }
