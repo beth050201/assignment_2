5110883137520cffc232e455a901c42190bc977b
beth050201 <56397955+beth050201@users.noreply.github.com>
2020-04-12 12:16:51 +0100

    JS

--- main.js
+++ main.js
@@ -1,24 +1,23 @@
 
-/* navigation*/
+/* navigation found by watching video - https://www.youtube.com/watch?v=gXkqy0b4M5g */
 
 /*equal to anonomous function*/
 const navSlide= ()=> {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
     const navLinks=document.querySelectorAll('.nav-links li');
- //Toggle Nav
+ //Toggle Navigation
     
     burger.addEventListener('click', () => {
         nav.classList.toggle('nav-active');
     });
-
 
   /* adding animation to the links in the navigation*/
     navLinks.forEach((link,index) =>{
        if(link.style.animation) {
            link.style.animation=''
         } else {
-          link.style.animation=`navLinkFade 0.5s ease forwards ${index /7 + 1.75}s`;
+          link.style.animation=`navLinkFade 0.5s ease forwards ${index /7 + 0.2}s`; // altering time delay 
         }
         /*shows the delay for each link*/
         console.log(index / 7);
@@ -31,6 +30,7 @@ navSlide();
 
 /* typing animation for the introduction on index page - https://codepen.io/LaithHaleem/pen/wzyrBd 
 began with lots of 'var' so changed to let to have  a more updated JavaScript form*/
+
 let string = "Hi I'm Bethany";
 let intro = string.split("");
 let el = document.getElementById('intro');
